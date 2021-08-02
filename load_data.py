b8440c5dc0a053da1406d3482e038c80187c3610
Roni Admon <radmon@microsoft.com>
2021-08-02 08:47:41 +0300

    x3

--- load_data.py
+++ load_data.py
@@ -12,7 +12,7 @@ x3_arr = np.load('z500'+fieldname) # geopotential height data (9*9 resolution)
 
 x1_arr_flat = stats.zscore(x1_arr.reshape([x1_arr.shape[0],x1_arr.shape[1]*x1_arr.shape[2]]))
 x2_arr_flat = stats.zscore(x2_arr.reshape([x2_arr.shape[0],x2_arr.shape[1]*x2_arr.shape[2]]))
-x3_arr_flat = stats.zscore(x2_arr.reshape([x2_arr.shape[0],x2_arr.shape[1]*x2_arr.shape[2]]))
+x3_arr_flat = stats.zscore(x2_arr.reshape([x3_arr.shape[0],x3_arr.shape[1]*x3_arr.shape[2]]))
 y_arr = np.load('rain__hourly_20090201-20191231.npy') # rain data
 
 tensor_x = torch.Tensor(np.concatenate([x1_arr_flat,x2_arr_flat,x3_arr_flat],axis=1))
